32e3f8822db835b8349578048744bacd92ced48e
Sara Blom <sara.cecilia.blom@gmail.com>
2022-08-30 16:27:21 +0200

    update packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
-import { HelloWorld } from "../src/components/HelloWorld";
+import type { AppProps } from "next/app";
 import "../public/css/styles.css";
 import React from "react";
+import { GlobalStyles } from "../src/styles/GlobalStyles";
+import { StylesReset } from "../src/styles/StyleReset";
+import { CssVariables } from "../src/styles/CssVariables";
 
-function HomePage() {
-  return (
-    <React.StrictMode>
-      <HelloWorld />
-    </React.StrictMode>
-  );
+function HomePage({ Component, pageProps }: AppProps) {
+    const AnyComponent = Component as any;
+    return (
+        <React.StrictMode>
+            <AnyComponent {...pageProps} />
+            <GlobalStyles />
+            <StylesReset />
+            <CssVariables />
+        </React.StrictMode>
+    );
 }
 
 export default HomePage;
