4bd7045d846bd68e8547db330c5baebd2ee27ebf
Sara Blom <sara.cecilia.blom@gmail.com>
2022-08-31 14:10:12 +0200

    add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,38 @@
-import { useState, useEffect } from "react";
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
 import { DisplayElectricCars } from "../src/components/DisplayElectricCars";
-import electricCars from "../public/api/cars.json";
-import useSWR from "swr";
-import { Car } from "../src/models/car";
 import { useGetCars } from "../src/hooks/useGetCars";
+import { Spinner, Icon, Flex } from "vcc-ui";
+import styled from "styled-components";
 
 const HomePage: NextPage = () => {
     const { cars, isLoading, isError } = useGetCars();
 
-    if (isLoading) return <p>Loading</p>;
+    if (isLoading) return <Spinner size={32} />;
     if (isError) return <p>Error</p>;
 
     return (
-        <>
+        <PageWrapper>
             <Head>
                 <title>Volvo Electric Cars</title>
             </Head>
             <DisplayElectricCars cars={cars} />
-        </>
+            <Icon type="more-48" color="primary" />
+            <Flex extend={{ flexDirection: "row" }}>
+                <Icon type="mediacircled-previous-48" />
+                <Icon type="mediacircled-next-48" />
+            </Flex>
+        </PageWrapper>
     );
 };
 
 export default HomePage;
+
+const PageWrapper = styled.main`
+    display: flex;
+    flex-direction: column;
+
+    img.ej {
+        align-self: center;
+    }
+`;
