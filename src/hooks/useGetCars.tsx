c358e2028780640139c9835f647e1573b9c30593
Sara Blom <sara.cecilia.blom@gmail.com>
2022-09-01 14:24:05 +0200

    add return data types

--- src/hooks/useGetCars.tsx
+++ src/hooks/useGetCars.tsx
@@ -1,13 +1,19 @@
 import useSWR from "swr";
 import { Car } from "../models/car";
 
+interface ReturnData {
+    cars: Car[];
+    isLoading: boolean;
+    isError: boolean;
+}
+
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
-export function useGetCars() {
+export function useGetCars(): ReturnData {
     const { data, error } = useSWR(`/api/cars.json`, fetcher);
 
     return {
-        cars: data as Car[],
+        cars: data,
         isLoading: !error && !data,
         isError: error,
     };
--- src/hooks/useGetSingleCar.tsx
+++ src/hooks/useGetSingleCar.tsx
@@ -1,9 +1,15 @@
 import useSWR from "swr";
 import { Car } from "../models/car";
 
+interface ReturnData {
+    car: Car;
+    isLoading: boolean;
+    isError: boolean;
+}
+
 const fetcher = (url: string) => fetch(url).then(res => res.json());
 
-export function useGetSingleCar(id: string | string[] | undefined) {
+export function useGetSingleCar(id: string | string[] | undefined): ReturnData {
     const { data, error } = useSWR(`/api/cars.json`, fetcher);
 
     let filteredCar;
