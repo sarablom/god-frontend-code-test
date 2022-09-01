412c0d96e91afe25bbeb47b15bed79093e2da5f4
Sara Blom <sara.cecilia.blom@gmail.com>
2022-09-01 14:23:36 +0200

    change styling template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from "next/app";
 import "../public/css/styles.css";
 import React from "react";
 import { StyleProvider, ThemePicker } from "vcc-ui";
-import { GlobalStyles } from "../src/styles/GlobalStyles";
-import { StylesReset } from "../src/styles/StyleReset";
 import { CssVariables } from "../src/styles/CssVariables";
 
 function HomePage({ Component, pageProps }: AppProps) {
@@ -13,8 +11,6 @@ function HomePage({ Component, pageProps }: AppProps) {
             <StyleProvider>
                 <ThemePicker variant="light">
                     <AnyComponent {...pageProps} />
-                    <GlobalStyles />
-                    <StylesReset />
                     <CssVariables />
                 </ThemePicker>
             </StyleProvider>
