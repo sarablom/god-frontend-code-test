5af018aa2bd49d5e5b080746ec9c2edc60b37f08
Sara Blom <sara.cecilia.blom@gmail.com>
2022-09-02 16:01:20 +0200

    add a filter function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { DisplayElectricCar } from "../src/components/DisplayElectricCar";
+import { ElectricCarCard } from "../src/components/ElectricCarCard";
 import { useGetCars } from "../src/hooks/useGetCar";
 import { Spinner } from "vcc-ui";
 import styled from "styled-components";
+import { FilterSearchBar } from "../src/components/FIlterSearchBar";
+import { Carousel } from "../src/components/Carousel";
+import { useState } from "react";
 
 const HomePage: NextPage = () => {
     const { cars, isLoading, isError } = useGetCars();
+    const [filteredCars, setFilteredCars] = useState(cars);
 
     if (isLoading) return <Spinner size={32} />;
     if (isError) return <p>Error</p>;
@@ -16,7 +20,16 @@ const HomePage: NextPage = () => {
             <Head>
                 <title>Volvo Electric Cars</title>
             </Head>
-            <DisplayElectricCar cars={cars} />          
+            <FilterSearchBar
+                cars={cars}
+                filteredCars={filteredCars}
+                setFilteredCars={setFilteredCars}
+            />
+            <Carousel>
+                {filteredCars.map(car => (
+                    <ElectricCarCard key={car.id} car={car} />
+                ))}
+            </Carousel>
         </PageWrapper>
     );
 };
