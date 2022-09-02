bcd2bcdbf47561495093b3fe7d723ed2deb5f56b
Sara Blom <sara.cecilia.blom@gmail.com>
2022-09-02 13:52:08 +0200

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,12 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import { DisplayElectricCar } from "../src/components/DisplayElectricCar";
-import { Carousel } from "../src/components/Carousel";
-import { useGetCars } from "../src/hooks/useGetCars";
-import useWindowSize from "../src/hooks/useWindowSize";
-import { Breakpoints } from "../src/styles/BreakPoints";
+import { useGetCars } from "../src/hooks/useGetCar";
 import { Spinner } from "vcc-ui";
 import styled from "styled-components";
-import { BackAndForwardButtons } from "../src/components/BackAndForwardButtons";
 
 const HomePage: NextPage = () => {
     const { cars, isLoading, isError } = useGetCars();
-    const { width } = useWindowSize();
-    const breakpoint = Number(Breakpoints.TabletLandscape.split("rem")[0]) * 16;
 
     if (isLoading) return <Spinner size={32} />;
     if (isError) return <p>Error</p>;
@@ -22,10 +16,7 @@ const HomePage: NextPage = () => {
             <Head>
                 <title>Volvo Electric Cars</title>
             </Head>
-            <Carousel>
-                <DisplayElectricCar cars={cars} />
-            </Carousel>
-            {width > breakpoint ? <BackAndForwardButtons /> : null}
+            <DisplayElectricCar cars={cars} />          
         </PageWrapper>
     );
 };
@@ -35,12 +26,4 @@ export default HomePage;
 const PageWrapper = styled.main`
     display: flex;
     flex-direction: column;
-
-    img.ej {
-        align-self: center;
-    }
-
-    .back-and-forward-button {
-        align-self: flex-end;
-    }
 `;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,27 +1,140 @@
-import React, { FC } from "react";
+import {
+    FC,
+    ReactNode,
+    useState,
+    cloneElement,
+    Children,
+    ReactChild,
+    ReactElement,
+} from "react";
+import { useSwipeable } from "react-swipeable";
 import styled from "styled-components";
+import { IndicatorButtons } from "./IndicatorButtons";
+import { ScreenReaderText } from "./ScreenReaderText";
+import { Breakpoints } from "../styles/BreakPoints";
+import { Flex, Icon } from "vcc-ui";
 
 interface Props {
-    children: React.ReactNode;
+    children: ReactNode;
 }
 
+//Hughly inspired by TinySo https://medium.com/tinyso/how-to-create-the-responsive-and-swipeable-carousel-slider-component-in-react-99f433364aa0
+
 export const Carousel: FC<Props> = ({ children }) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const updateIndex = (newIndex: number) => {
+        if (newIndex < 0) {
+            newIndex = 0;
+        } else if (newIndex >= Children.count(children)) {
+            newIndex = Children.count(children) - 1;
+        }
+
+        setActiveIndex(newIndex);
+    };
+
+    const handlers = useSwipeable({
+        onSwipedLeft: () => updateIndex(activeIndex + 1),
+        onSwipedRight: () => updateIndex(activeIndex - 1),
+    });
+
     return (
-        <CarouselWrapper className="carousel">
-            <InnerChild style={{ transform: "translateX(-0%" }}>
-                {React.Children.map(children, (child: any, index) => {
-                    return React.cloneElement(child, { width: "100%" });
+        <CarouselWrapper {...handlers} className="carousel">
+            {/*TODO: Need to change this calculation */}
+            <InnerChild
+                style={{ transform: `translateX(-${activeIndex * 89}%)` }}
+            >
+                {Children.map(children, (child: any, index) => {
+                    return cloneElement(child, { width: "100%" });
                 })}
             </InnerChild>
+            <Indicators>
+                <Flex
+                    extend={{ flexDirection: "row", alignItems: "flex-end" }}
+                    className="previous-and-next-button"
+                >
+                    <button
+                        onClick={() => {
+                            updateIndex(activeIndex - 1);
+                        }}
+                        className="prev-button"
+                    >
+                        <ScreenReaderText text="Previous" />
+                        <Icon type="mediacircled-previous-48" />
+                    </button>
+                    <button
+                        onClick={() => {
+                            updateIndex(activeIndex + 1);
+                        }}
+                        className="next-button"
+                    >
+                        <ScreenReaderText text="Next" />
+                        <Icon type="mediacircled-next-48" />
+                    </button>
+                </Flex>
+                {Children.map(children, (_, index) => {
+                    return (
+                        <IndicatorButtons
+                            index={index}
+                            updateIndex={updateIndex}
+                        />
+                    );
+                })}
+            </Indicators>
         </CarouselWrapper>
     );
 };
 
 const CarouselWrapper = styled.div`
     overflow: hidden;
+    display: flex;
+    flex-direction: column;
 `;
 
 const InnerChild = styled.div`
     white-space: nowrap;
     transition: transform 0.3s;
+    scroll-snap-type: x mandatory;
+`;
+
+const Indicators = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .indicator-buttons {
+        height: 16px;
+        width: 16px;
+        cursor: pointer;
+        margin: 8px;
+        border-radius: 50%;
+        border: none;
+        color: #fafafa;
+
+        &:active,
+        &:focus {
+            background: #141414;
+        }
+
+        @media screen and (min-width: ${Breakpoints.TabletPortrait}) {
+            display: none;
+        }
+    }
+
+    .previous-and-next-button {
+        justify-self: end;
+        margin-left: auto;
+        margin-right: 32px;
+
+        @media screen and (max-width: ${Breakpoints.TabletPortrait}) {
+            display: none;
+        }
+    }
+
+    .prev-button,
+    .next-button {
+        cursor: pointer;
+        border: none;
+        background: inherit;
+    }
 `;
