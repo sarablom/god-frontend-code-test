7435595b5fcbfc9edcba5dcd911a0a92c77958a9
Sara Blom <sara.cecilia.blom@gmail.com>
2022-09-02 13:47:43 +0200

    change styling

--- src/components/DisplayElectricCar.tsx
+++ src/components/DisplayElectricCar.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { Car } from "../models/car";
 import { Flex, Text } from "vcc-ui";
 import styled from "styled-components";
+import { Carousel } from "./Carousel";
+import { Breakpoints } from "../styles/BreakPoints";
 
 interface Props {
     cars: Car[];
@@ -11,7 +13,7 @@ interface Props {
 
 export const DisplayElectricCar: FC<Props> = ({ cars }) => {
     return (
-        <Flex extend={{ flexDirection: "row" }}>
+        <Carousel>
             {cars.map(car => (
                 <Card key={car.id} className="carousel-item">
                     <Text
@@ -24,29 +26,41 @@ export const DisplayElectricCar: FC<Props> = ({ cars }) => {
                     >
                         {car.bodyType.toUpperCase()}
                     </Text>
-
-                    <Text variant="amundsen">{car.modelName} </Text>
-
-                    <Text
-                        variant="columbus"
-                        extend={({ theme }) => ({
-                            fontSize: "1rem",
-                            color: theme.color.foreground.secondary,
-                            fontWeight: "500",
-                        })}
+                    <Flex
+                        extend={{ flexDirection: "row" }}
+                        className="text-warp"
                     >
-                        {car.modelType}
-                    </Text>
+                        <Text variant="amundsen">{car.modelName}</Text>
+
+                        <Text
+                            variant="columbus"
+                            extend={({ theme }) => ({
+                                fontSize: "1rem",
+                                color: theme.color.foreground.secondary,
+                                fontWeight: "500",
+                                marginLeft: "8px",
+                            })}
+                        >
+                            {car.modelType}
+                        </Text>
+                    </Flex>
 
                     <Image
                         src={car.imageUrl}
                         alt={car.modelName}
                         width="100%"
-                        height="100%"
+                        height="80%"
                         layout="responsive"
                         objectFit="contain"
+                        className="electric-car-image"
                     />
-                    <Flex extend={{ flexDirection: "row" }}>
+                    <Flex
+                        className="flex-center"
+                        extend={{
+                            flexDirection: "row",
+                            justityContent: "center",
+                        }}
+                    >
                         <LinkWithRightArrow
                             name="Learn"
                             path={`/learn/${car.id}`}
@@ -58,13 +72,32 @@ export const DisplayElectricCar: FC<Props> = ({ cars }) => {
                     </Flex>
                 </Card>
             ))}
-        </Flex>
+        </Carousel>
     );
 };
 
 const Card = styled.div`
-    min-width: 350px;
-    margin: 16px;
+    width: 80%;
+    padding: 16px;
     display: inline-flex;
     flex-direction: column;
+    scroll-snap-align: center;
+
+    @media screen and (min-width: ${Breakpoints.TabletPortrait}) {
+        width: 350px;
+    }
+
+    .flex-center {
+        justify-content: center;
+    }
+
+    .electric-car-image {
+        align-self: center;
+    }
+
+    .text-warp {
+        @media screen and (max-width: ${Breakpoints.TabletPortrait}) {
+            flex-direction: column;
+        }
+    }
 `;
